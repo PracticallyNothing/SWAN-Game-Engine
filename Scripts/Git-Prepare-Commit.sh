9856ed0c12497a6f904fbaa1a31b1f22cbb5c3b8
Practically Nothing <practicallynothingatall@gmail.com>
2017-07-28 11:16:47 +0300

    Added utility script

---
+++ Scripts/Git-Prepare-Commit.sh
@@ -0,0 +1,13 @@
+git add CMakeLists.txt
+
+git add Scripts/*
+
+git add Source/Main.cpp
+git add Source/CMakeConfig.h.in
+
+git add Source/Core/*
+git add GUI/*
+git add Logic/*
+git add Physics/*
+git add Rendering/*
+git add Utility/*
